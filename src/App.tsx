6edfffd167b2bc402ae6c7378dfd02f1e4206f4f
Andy Freeborough <andy@freeborough.com>
2025-11-28 19:00:56 +0000

    Added a suitable shadcn theme.
    Dark-only orange for now!  We can add in the light variant with a toggle in a future phase.
    
    Created a more suitable landing page - it'll do for now!

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl">Track and Store</h1>
-      <Button className="mt-6">Get Started</Button>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div>
+          <h1 className="text-primary text-5xl xl:text-7xl font-bold">Track and Store</h1>
+          <p className="m-6">Never lose track of your things again.</p>
+          <div className="mt-6">
+            <Button className="mr-4">Get Started</Button>
+            <Button variant={'secondary'}>Learn More</Button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
